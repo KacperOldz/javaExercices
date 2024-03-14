ccd997a9b2af82e5e66eaa5e8c6352dbe088421b
Kacper <87307561+KacperOldz@users.noreply.github.com>
2024-03-14 17:44:50 +0100

    Create myRunnable.java

---
+++ threads/myRunnable.java
@@ -0,0 +1,6 @@
+public class myRunnable implements Runnable {
+  public void run() {
+    // Code that will be executed at the thread when it's created like that
+    // Thread threadName = new Thread( [Here should be object of myRunnable], "threadName")
+  }
+}
