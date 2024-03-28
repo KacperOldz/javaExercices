e6d018a821f96cda5434ee312b3c46172bb87dbf
Kacper <87307561+KacperOldz@users.noreply.github.com>
2024-03-28 11:55:56 +0100

    Create methodReferences.java

---
+++ functionalProgramming/methodReferences.java
@@ -0,0 +1,48 @@
+package pl.clockworkjava;
+
+import pl.clockworkjava.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Student> students = createData();
+        Supplier<List<Student>> studentsSupplier = Main::createData;
+
+        Consumer<String> printName = System.out::println;
+        Consumer<String> printNameUppercase = Main::testMe;
+
+        Function<Student, String> getStudentName = Student::getName;
+
+        BiFunction<Student,String,Student> changeIndex = Student::changeIndexNumber;
+
+        consumeStudents(studentsSupplier, printNameUppercase.andThen(printName), getStudentName);
+    }
+
+    private static void consumeStudents(Supplier<List<Student>> supplier, Consumer<String> consumer, Function<Student, String> function) {
+        for(Student s: supplier.get()) {
+            consumer.accept(function.apply(s));
+        }
+    }
+
+    private static List<Student> createData() {
+        List<Student> result = new ArrayList<>();
+
+        result.add(new Student("Kacper", 17, "01U"));
+        result.add(new Student("Maciek", 31, "02U"));
+        result.add(new Student("Wojtek", 21, "03U"));
+
+        return result;
+
+    }
+
+    private static void testMe(String x) {
+        System.out.println(x.toUpperCase());
+    }
+}
